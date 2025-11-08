50af4008187b2d6b9a19254ad81b6c635641095b
SOUROYAL123 <souravrchowdhury2020@gmail.com>
2025-11-08 20:39:26 +0530

    Update start-n8n.sh

--- start-n8n.sh
+++ start-n8n.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 export DB_TYPE=postgresdb
 export DB_POSTGRESDB_URL="postgresql://n8n_bxpw_user:aminty@123weQ45637@dpg-d3i8ihidbo4c73fh3ne0-a.singapore-postgres.render.com:5432/n8n_bxpw?sslmode=require"
 export N8N_PORT=5678
