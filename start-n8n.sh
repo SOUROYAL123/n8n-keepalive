04120d6274c2df5a6d25adb54be9938df2368b84
SOUROYAL123 <souravrchowdhury2020@gmail.com>
2025-11-08 20:19:31 +0530

    Add start-n8n.sh script for n8n setup

---
+++ start-n8n.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+export DB_TYPE=postgresdb
+export DB_POSTGRESDB_URL="postgresql://n8n_bxpw_user:aminty@123weQ45637@dpg-d3i8ihidbo4c73fh3ne0-a.singapore-postgres.render.com:5432/n8n_bxpw?sslmode=require"
+export N8N_PORT=5678
+n8n start
